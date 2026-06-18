cc2c8b9639f2e0f13a47eab5db241cf624c97fb1
ihadj <ihadj@f6r11s7.paris.42.school>
2026-06-18 12:21:09 +0200

    fixed

--- src/backend/controllers/authController.ts
+++ src/backend/controllers/authController.ts
@@ -19,26 +19,26 @@ function  isValidPassword(password: string): boolean {
   return passwordRegex.test(password);
 }
 
-function validateFormData(data: Partial<UserInterface>): boolean {
+function validateFormData(data: Partial<UserInterface>): { isValid: boolean; reason?: string } {
   const { email, username, password } = data;
   
   if (!email || !isValidEmail(email))
-    return false;
+    return { isValid: false, reason: "Adresse e-mail invalide." };
   if (!username || !isValidUsername(username))
-    return false;
+    return { isValid: false, reason: "Nom d'utilisateur invalide. Il doit contenir entre 3 et 20 caractères alphanumériques ou underscores." };
   if (!password || !isValidPassword(password))
-    return false;
+    return { isValid: false, reason: "Mot de passe invalide. Il doit contenir au moins 5 caractères, dont au moins une lettre et un chiffre." };
   
-  return true;
+  return { isValid: true };
 }
 
 export async function registerController(req: FastifyRequest, reply: FastifyReply)
 {
   let newUser: UserInterface = req.body as UserInterface;
 
-  if (!validateFormData(newUser))
-    return reply.code(400).send({ error: "Données de formulaire invalides." });
-  
+  if (validateFormData(newUser).isValid === false)
+    return reply.code(400).send({ error: validateFormData(newUser).reason });
+
   const validEmail = await prisma.user.findUnique({where :{email:newUser.email}})
   if (validEmail)
 		return reply.code(409).send({ error: "Cet e-mail est déjà utilisé." });
@@ -91,14 +91,14 @@ export async function loginController(req : FastifyRequest, reply : FastifyReply
     });
     return (reply.send({token, user}));
   }catch (error) {
-    return (reply.code(401).send({ message: "Identifiants de connexion invalides." }));
+    return (reply.code(401).send({ error: "Identifiants de connexion invalides." }));
   }
 }
 
 export async function logoutController(req : FastifyRequest, reply: FastifyReply){
   const token : string | undefined = req.cookies.token;
   if (token === undefined)
-      return (reply.code(400).send({ message: "Pas d'utilisateur connecté." }))
+      return (reply.code(400).send({ error: "Pas d'utilisateur connecté." }))
   try {
     const decoded = req.server.jwt.decode<JwtPayload>(token);
     await prisma.blacklistedToken.create({
@@ -113,8 +113,8 @@ export async function logoutController(req : FastifyRequest, reply: FastifyReply
       sameSite: 'strict',
       path: '/'
     });
-    return (reply.send({ message: 'Déconnecté.' }));
+    return (reply.send({ error: 'Déconnecté.' }));
   }catch (error){
-    return (reply.code(400).send({message: "Json web token invalide."}))
+    return (reply.code(400).send({error: "Json web token invalide."}))
   }
 }
