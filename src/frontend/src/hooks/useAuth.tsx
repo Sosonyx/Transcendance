aafb384c74be37bbf0a637f12140513afc5882b0
Thermow <teochevallier20@gmail.com>
2026-06-18 14:29:40 +0200

    temp commit

--- src/frontend/src/hooks/useAuth.tsx
+++ src/frontend/src/hooks/useAuth.tsx
@@ -4,9 +4,12 @@ import type { User } from "../types/types.js";
 
 export function useAuth() {
   const [user, setUser] = useState<User | null>(null); //useState(InitialValue) -> creer une variable readOnly, modifiable avec la fct setState, React rerender lors que cest modifier
-  const [loading, setLoading] = useState(true); //Loading/Submitting dans toutes les fct font qui font des calls API (Pas rerender avec des etats intermediaire)
+  const [loading, setLoading] = useState(false); //Loading/Submitting dans toutes les fct font qui font des calls API (Pas rerender avec des etats intermediaire)
 
   const refreshAuth = async () => {
+    const token = document.cookie.split(";").find(row => row.startsWith("token"));
+    if (!token)
+        return ;
     setLoading(true);
 
     try {
