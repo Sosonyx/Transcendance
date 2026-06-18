eeabe1506127f24a08f3b81fdbbd996f3297bb68
ihadj <ihadj@f6r11s7.paris.42.school>
2026-06-18 17:59:16 +0200

    stable version

--- src/backend/entrypoint.sh
+++ src/backend/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 npx prisma db push --schema=backend/prisma/schema.prisma
-npx prisma studio --port 5555 --browser none --schema=backend/prisma/schema.prisma &
 exec node build/backend/server.js
